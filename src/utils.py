e6b0d11dc889593bf195377f266b16eb17d49de6
soumyak1288 <97076800+soumyak1288@users.noreply.github.com>
2024-10-10 11:06:43 +0530

    model_trainer complete

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -15,6 +16,40 @@ def save_object(file_path,obj):
         
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise  CustomException("Error occurred while saving the object",e,sys) # type:ignore
+            
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            model.fit(X_train,y_train)
+            
+            # para=param[list(models.keys())[i]]
+            
+
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
+            # model.fit(X_train,y_train)
+
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
             
     except Exception as e:
         raise  CustomException("Error occurred while saving the object",e,sys) # type:ignore
